5826635ef67ee38fa5422eb159a398072b68666e
silverback23 <cahid23@hotmail.com>
2021-11-05 13:51:23 +0300

    first commit

---
+++ S1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+namespace vs2
+{
+    class Program
+    {
+        ArrayList Toplam=new ArrayList();
+        static void Main(string[] args)
+        {
+            int sayi;
+           
+            ArrayList Toplam=new ArrayList();
+            
+            
+            Console.WriteLine("İstediğiniz sayıları giriniz.");
+            
+            for ( ; ;)
+            {
+                sayi=Convert.ToInt32(Console.ReadLine());
+                if(sayi>0)
+                 if(sayi%2==0)
+                 {
+                  Toplam.Add(sayi);
+                 }
+   
+                if(sayi<0)
+                {
+                    Console.WriteLine("lütfen pozitif bir sayı giriniz");
+                    continue;
+              }
+              if(sayi==0)
+              {
+                Console.WriteLine("Çift Sayılar :  ");
+                foreach (var eleman in Toplam)
+                {
+                    Console.WriteLine(eleman);
+                }
+              }
+              
+            }
+            
+        }
+    }
+}
